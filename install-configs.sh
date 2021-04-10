9bf86a159ddbf0529977d4bb3ae90293a3629ab8
Tyrone <ty.cx123@gmail.com>
2021-04-10 17:16:00 -0400

    Add alacritty
    Set alacritty as default terminal and add its configurations

--- install-configs.sh
+++ install-configs.sh
@@ -6,5 +6,8 @@ dst="$HOME/.config/"
 mkdir -p $dst
 
 for config in `ls config/`; do
-  ln -s $src/config/$config $dst/$config
+  if [ ! -d $dst/$config ]; then
+    echo "Installing $config..."
+    ln -s $src/config/$config $dst/$config
+  fi
 done
