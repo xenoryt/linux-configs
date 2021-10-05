bcef57a0039ede601c87adba6a52a755bda14eff
Tyrone Xiong <tyrone.xiong@caseware.com>
2021-10-05 17:14:21 -0400

    Update nvim-tree configs

--- config/nvim/lua/plugin/nvim-tree.lua
+++ config/nvim/lua/plugin/nvim-tree.lua
@@ -37,7 +37,6 @@ vim.g["nvim_tree_icons"] = {
     }
 
 
-
 -- following options are the default
 require'nvim-tree'.setup {
   -- disables netrw completely
@@ -53,10 +52,15 @@ require'nvim-tree'.setup {
   -- opens the tree when changing/opening a new tab if the tree wasn't previously opened
   open_on_tab         = false,
   -- hijacks new directory buffers when they are opened.
-  update_to_buf_dir   = true,
+  update_to_buf_dir   = {
+    -- enable the feature
+    enable = true,
+    -- allow to open the tree if it was previously closed
+    auto_open = true,
+  },
   -- hijack the cursor in the tree to put it at the start of the filename
   hijack_cursor       = false,
-  -- updates the root directory of the tree on `DirChanged` (when your run `:cd` usually) 
+  -- updates the root directory of the tree on `DirChanged` (when your run `:cd` usually)
   update_cwd          = false,
   -- show lsp diagnostics in the signcolumn
   lsp_diagnostics     = false,
@@ -80,8 +84,10 @@ require'nvim-tree'.setup {
   },
 
   view = {
-    -- width of the window, can be either a number (columns) or a string in `%`
+    -- width of the window, can be either a number (columns) or a string in `%`, for left or right side placement
     width = 30,
+    -- height of the window, can be either a number (columns) or a string in `%`, for top or bottom side placement
+    height = 30,
     -- side of the tree, can be one of 'left' | 'right' | 'top' | 'bottom'
     side = 'left',
     -- if true the tree will resize itself after opening a file
