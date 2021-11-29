0d35b785f9553fd3613d4da259343d2c7ba71a1e
Tyrone Xiong <tyrone.xiong@caseware.com>
2021-11-29 08:43:05 -0500

    Update monitor setup

--- config/bspwm/autostart.sh
+++ config/bspwm/autostart.sh
@@ -15,7 +15,7 @@ function run {
 
 xrandr \
   --output eDP --primary --mode 1920x1080 --pos 1920x0 --rotate normal \
-  --output DisplayPort-0 --mode 1920x1200 --pos 0x0 --rotate normal
+  --output DisplayPort-1 --mode 1920x1200 --pos 0x0 --rotate normal
 
 # $HOME/.config/polybar/launch.sh &
 #xfsettingsd &
