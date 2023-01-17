fcdbeddcc54222c9a611370301cf88df729c01c6
Tyrone Xiong <xenoryt@duck.com>
2023-01-17 15:35:56 -0500

    Treat terraform-vars same as terraform

--- config/nvim/lua/plugin/nvim-treesitter.lua
+++ config/nvim/lua/plugin/nvim-treesitter.lua
@@ -1,14 +1,14 @@
 local parser_configs = require('nvim-treesitter.parsers').get_parser_configs()
 
 parser_configs.norg = {
-    install_info = {
-        url = "https://github.com/nvim-neorg/tree-sitter-norg",
-        files = { "src/parser.c", "src/scanner.cc" },
-        branch = "main"
-    },
+  install_info = {
+    url = "https://github.com/nvim-neorg/tree-sitter-norg",
+    files = { "src/parser.c", "src/scanner.cc" },
+    branch = "main"
+  },
 }
 
-require'nvim-treesitter.configs'.setup {
+require 'nvim-treesitter.configs'.setup {
   highlight = {
     enable = true,
     additional_vim_regex_highlighting = false,
@@ -35,11 +35,11 @@ require'nvim-treesitter.configs'.setup {
     'typescript',
     'vim',
   },
-  indent = {enable = true},
-  autopairs = {enable = true},
-  rainbow = {enable = true},
-  autotag = {enable = true},
-  context_commentstring = {enable = true},
+  indent = { enable = true },
+  autopairs = { enable = true },
+  rainbow = { enable = true },
+  autotag = { enable = true },
+  context_commentstring = { enable = true },
 
   incremental_selection = {
     enable = true,
@@ -71,3 +71,4 @@ require'nvim-treesitter.configs'.setup {
     },
   },
 }
+require("nvim-treesitter.parsers").filetype_to_parsername["terraform-vars"] = "hcl"
