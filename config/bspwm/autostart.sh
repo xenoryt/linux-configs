9eccfa7aeaec47a74d6131db9a13d20e723e37a2
Tyrone <ty.cx123@gmail.com>
2023-12-08 17:47:53 -0500

    bspwm updates

--- config/bspwm/autostart.sh
+++ config/bspwm/autostart.sh
@@ -13,32 +13,42 @@ function run {
 #xrandr --output LVDS1 --mode 1366x768 --output DP3 --mode 1920x1080 --right-of LVDS1
 #xrandr --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
 
-xrandr \
-  --output eDP --primary --mode 1920x1080 --pos 1920x0 --rotate normal \
-  --output HDMI-A-0 --mode 1920x1200 --pos 0x0 --rotate normal
+#xrandr \
+#  --output eDP --primary --mode 1920x1080 --pos 1920x0 --rotate normal \
+#  --output HDMI-A-0 --mode 1920x1200 --pos 0x0 --rotate normal
+
+if [ "$(xrandr | grep -E "(HDMI|DP)-0 connected" | wc -l)" -eq 2 ]; then
+  xrandr \
+    --output HDMI-0 --primary --mode 1920x1200 --pos 0x50 --rotate normal \
+    --output DP-0 --mode 2560x1440 --pos 1920x0 --rotate normal
+
+  bspc monitor DP-0   -d 1 2 3 4 5
+  bspc monitor HDMI-0 -d A B C
+fi
+
 
 # $HOME/.config/polybar/launch.sh &
 #xfsettingsd &
-xfce4-panel &
+#xfce4-panel &
 
-~/.config/bspwm/scripts/disable-touchscreen.sh &
+#~/.config/bspwm/scripts/disable-touchscreen.sh &
 
 xsetroot -cursor_name left_ptr &
 sxhkd &
 
-feh --bg-fill --randomize ~/Pictures/wallpapers/**
+feh --bg-fill --randomize /usr/share/backgrounds/**
 
 #conky -c $HOME/.config/bspwm/system-overview &
 #run variety &
 run nm-applet &
 run nextcloud --background &
 #run pamac-tray &
-run xfce4-power-manager &
+#run xfce4-power-manager &
 #numlockx on &
 #blueberry-tray &
 #compton --config $HOME/.config/bspwm/compton.conf &
 #/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-/usr/lib/xfce4/notifyd/xfce4-notifyd &
+#/usr/lib/xfce4/notifyd/xfce4-notifyd &
 #nitrogen --restore &
 #run caffeine &
 #run vivaldi-stable &
